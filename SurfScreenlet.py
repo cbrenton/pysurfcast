d56cb9be017090b74ddcc3cabf81e733d392a266
Chris Brenton <cbrenton@calpoly.edu>
2011-08-30 11:34:38 -0700

    First commit. Working xml retrieval, data file reading/writing, request limiting.

---
+++ SurfScreenlet.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env/python
+
+import os.path
+import time
+from datetime import date
+import urllib
+from xml.etree import ElementTree as ET
+
+feedUrl = "http://www.spitcast.com/api/spot/forecast/147/?dcat=week&format=xml"
+feedPath = "data.xml"
+
+# Determine if the data file exists and is up to date.
+def hasCurrentData():
+    # IF file exists
+    if os.path.isfile(feedPath):
+        # Get modified date of file.
+        modDate = date.fromtimestamp(os.path.getmtime(feedPath))
+        # Get today's date.
+        today = date.today();
+        # IF the modified date is before today's date
+        if today > modDate:
+            return False
+        else:
+            return True
+
+def main():
+    # IF forecast data is current.
+    if hasCurrentData():
+        print "forecast file exists and is up to date"
+        # Parse the forecast data from the file.
+        element = ET.parse(feedPath)
+    # IF forecast data is from a previous day.
+    else:
+        print "retrieving forecast data from url"
+        # Retrieve feed data.
+        feed = urllib.urlopen(feedUrl)
+        # Parse the retrieved forecast data.
+        element = ET.parse(feed)
+        # Write the data to the data file.
+        element.write(feedPath)
+    # Make list of all FORECAST elements in feed data.
+    forecastList = element.getroot().findall("FORECAST")
+    # FOR each forecast element
+    for forecast in forecastList:
+        # Find the SIZE_MAXIMUM element in the current forecast.
+        maxSize = forecast.find("SIZE_MAXIMUM").text
+        minSize = forecast.find("SIZE_MINIMUM").text
+        shape = forecast.find("SHAPE").text
+        hour = forecast.find("HOUR").text
+        day = forecast.find("DAY").text
+        print "Hour: %s on %s\nSize: %s - %s\nShape: %s\n" % \
+                (hour, day, minSize, maxSize, shape)
+
+if __name__ == "__main__":
+    main()
